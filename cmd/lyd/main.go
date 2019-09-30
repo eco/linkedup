24bb83d3f509c91bd39125747e043e8d9107284a
Hamdi Allam <hallam@twitter.com>
2019-09-29 23:15:22 -0700

    cleaned up lyd file

--- cmd/lyd/main.go
+++ cmd/lyd/main.go
@@ -1,37 +1,19 @@
 package main
 
 import (
-	"encoding/json"
-	app "github.com/eco/longy"
-	"io"
-
 	"github.com/cosmos/cosmos-sdk/server"
-	"github.com/cosmos/cosmos-sdk/x/genaccounts"
-	genaccscli "github.com/cosmos/cosmos-sdk/x/genaccounts/client/cli"
-	"github.com/cosmos/cosmos-sdk/x/staking"
-
+	app "github.com/eco/longy"
 	"github.com/spf13/cobra"
+	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/libs/cli"
 	"github.com/tendermint/tendermint/libs/log"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	genutilcli "github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
-	abci "github.com/tendermint/tendermint/abci/types"
-	tmtypes "github.com/tendermint/tendermint/types"
 	dbm "github.com/tendermint/tm-db"
+	"io"
 )
 
 func main() {
 	cobra.EnableCommandSorting = false
-
 	cdc := app.MakeCodec()
-
-	config := sdk.GetConfig()
-	config.SetBech32PrefixForAccount(sdk.Bech32PrefixAccAddr, sdk.Bech32PrefixAccPub)
-	config.SetBech32PrefixForValidator(sdk.Bech32PrefixValAddr, sdk.Bech32PrefixValPub)
-	config.SetBech32PrefixForConsensusNode(sdk.Bech32PrefixConsAddr, sdk.Bech32PrefixConsPub)
-	config.Seal()
-
 	ctx := server.NewDefaultContext()
 
 	rootCmd := &cobra.Command{
@@ -39,20 +21,8 @@ func main() {
 		Short:             "longy the longest chain validator daemon",
 		PersistentPreRunE: server.PersistentPreRunEFn(ctx),
 	}
-	// CLI commands to initialize the chain
-	rootCmd.AddCommand(
-		genutilcli.InitCmd(ctx, cdc, app.ModuleBasics, app.DefaultNodeHome),
-		genutilcli.CollectGenTxsCmd(ctx, cdc, genaccounts.AppModuleBasic{}, app.DefaultNodeHome),
-		genutilcli.GenTxCmd(
-			ctx, cdc, app.ModuleBasics, staking.AppModuleBasic{},
-			genaccounts.AppModuleBasic{}, app.DefaultNodeHome, app.DefaultCLIHome,
-		),
-		genutilcli.ValidateGenesisCmd(ctx, cdc, app.ModuleBasics),
-		// AddGenesisAccountCmd allows users to add accounts to the genesis file
-		genaccscli.AddGenesisAccountCmd(ctx, cdc, app.DefaultNodeHome, app.DefaultCLIHome),
-	)
 
-	server.AddCommands(ctx, cdc, rootCmd, newApp, exportAppStateAndTMValidators)
+	server.AddCommands(ctx, cdc, rootCmd, newApp, nil)
 
 	// prepare and add flags
 	executor := cli.PrepareBaseCmd(rootCmd, "LY", app.DefaultNodeHome)
@@ -64,20 +34,4 @@ func main() {
 
 func newApp(logger log.Logger, db dbm.DB, traceStore io.Writer) abci.Application {
 	return app.NewLongyApp(logger, db)
-}
-
-func exportAppStateAndTMValidators(
-	logger log.Logger, db dbm.DB, traceStore io.Writer, height int64, forZeroHeight bool, jailWhiteList []string,
-) (json.RawMessage, []tmtypes.GenesisValidator, error) {
-
-	nsApp := app.NewLongyApp(logger, db)
-	if height != -1 {
-		err := nsApp.LoadHeight(height)
-		if err != nil {
-			return nil, nil, err
-		}
-		return nsApp.ExportAppStateAndValidators(forZeroHeight, jailWhiteList)
-	}
-
-	return nsApp.ExportAppStateAndValidators(forZeroHeight, jailWhiteList)
 }
