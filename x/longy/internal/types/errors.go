becd7a5c2862c8acc2472f85d4b3fa94eb4a59a8
Hamdi Allam <hallam@twitter.com>
2019-09-29 23:49:43 -0700

    reward dne error

--- x/longy/internal/types/errors.go
+++ x/longy/internal/types/errors.go
@@ -10,13 +10,20 @@ const (
 
 	QRCodeDoesNotExist   sdk.CodeType = 101
 	AttendeeDoesNotExist sdk.CodeType = 102
+	RewardDoesNotExist   sdk.CodeType = 103
 )
 
 // ErrQRCodeDoesNotExist is the error for when a QR code does not exist in our keeper
 func ErrQRCodeDoesNotExist() sdk.Error {
-	return sdk.NewError(DefaultCodespace, QRCodeDoesNotExist, "Name does not exist")
+	return sdk.NewError(DefaultCodespace, QRCodeDoesNotExist, "name does not exist")
 }
 
+// ErrAttendeeDoesNotExist indicates an id with not corresponding attendee
 func ErrAttendeeDoesNotExist() sdk.Error {
-	return sdk.NewError(DefaultCodespace, AttendeeDoesNotExist, "Attendee does not exist")
+	return sdk.NewError(DefaultCodespace, AttendeeDoesNotExist, "attendee does not exist")
+}
+
+// ErrRewardDoesNotExist indicates a reward type that doesn't exist
+func ErrRewardDoesNotExist() sdk.Error {
+	return sdk.NewError(DefaultCodespace, RewardDoesNotExist, "reward does note exist")
 }
