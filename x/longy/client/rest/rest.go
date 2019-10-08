9ebb56812a974e8ba189f58395a6350292e77602
StoyanD <s.dimitrov19@gmail.com>
2019-10-07 20:26:26 -0700

    -clean up

--- x/longy/client/rest/rest.go
+++ x/longy/client/rest/rest.go
@@ -18,17 +18,11 @@ const (
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, storeName string) {
 	//longy/attendees/{attendee_id}
 	r.HandleFunc(fmt.Sprintf("/%s/%s/{%s}", storeName, keeper.QueryAttendees, AttendeeIdKey), attendeeHandler(cliCtx, storeName)).Methods("GET")
-	//longy/attendees/address/{attendee_id}
+	//longy/attendees/address/{address_id}
 	r.HandleFunc(fmt.Sprintf("/%s/%s/%s/{%s}", storeName, keeper.QueryAttendees, keeper.AddressKey, AddressIdKey), attendeeAddressHandler(cliCtx, storeName)).Methods("GET")
 	//longy/scans/{scan_id}
 	r.HandleFunc(fmt.Sprintf("/%s/%s/{%s}", storeName, keeper.QueryScans, ScanIdKey), scanGetHandler(cliCtx, storeName)).Methods("GET")
 
 	//open endpoint to post transactions directly to full node
 	r.HandleFunc("/longy/txs", rest.BroadcastTxRequest(cliCtx)).Methods("POST")
-
-	//r.HandleFunc(fmt.Sprintf("/%s/names", storeName), buyNameHandler(cliCtx)).Methods("POST")
-	//r.HandleFunc(fmt.Sprintf("/%s/names", storeName), setNameHandler(cliCtx)).Methods("PUT")
-	//r.HandleFunc(fmt.Sprintf("/%s/names/{%s}", storeName, restName), resolveNameHandler(cliCtx, storeName)).Methods("GET")
-	//r.HandleFunc(fmt.Sprintf("/%s/names/{%s}/whois", storeName, restName), whoIsHandler(cliCtx, storeName)).Methods("GET")
-	//r.HandleFunc(fmt.Sprintf("/%s/names", storeName), deleteNameHandler(cliCtx)).Methods("DELETE")
 }
