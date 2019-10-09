6b83972daaac3c659820a93365bde19cdd6efb0f
StoyanD <s.dimitrov19@gmail.com>
2019-10-09 15:41:38 -0700

    -tmp

--- x/longy/client/rest/rest.go
+++ x/longy/client/rest/rest.go
@@ -23,12 +23,16 @@ func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, storeName string) 
 	//longy/attendees/{attendee_id}
 	r.HandleFunc(fmt.Sprintf("/%s/%s/{%s}", storeName, keeper.QueryAttendees, AttendeeIDKey),
 		attendeeHandler(cliCtx, storeName)).Methods("GET")
+
 	//longy/attendees/address/{address_id}
 	r.HandleFunc(fmt.Sprintf("/%s/%s/%s/{%s}", storeName, keeper.QueryAttendees, keeper.AddressKey,
 		AddressIDKey), attendeeAddressHandler(cliCtx, storeName)).Methods("GET")
+
 	//longy/scans/{scan_id}
 	r.HandleFunc(fmt.Sprintf("/%s/%s/{%s}", storeName, keeper.QueryScans, ScanIDKey),
 		scanGetHandler(cliCtx, storeName)).Methods("GET")
+
+	//longy/prizes
 
 	//open endpoint to post transactions directly to full node
 	r.HandleFunc("/longy/txs", rest.BroadcastTxRequest(cliCtx)).Methods("POST")
