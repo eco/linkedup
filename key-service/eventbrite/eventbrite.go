ab49f1ffa69e7414fcaa40a9adca6fe400952a94
Hamdi Allam <hamdi.allam97@gmail.com>
2019-10-10 00:56:05 -0700

    added polling

--- key-service/eventbrite/eventbrite.go
+++ key-service/eventbrite/eventbrite.go
@@ -3,6 +3,8 @@ package eventbrite
 import (
 	"github.com/eco/longy/eventbrite"
 	"github.com/sirupsen/logrus"
+	"sync"
+	"time"
 )
 
 var (
@@ -11,7 +13,11 @@ var (
 
 // Session to hook into the eventbrite API
 type Session struct {
-	attendees map[int]eventbrite.AttendeeProfile
+	sync.Mutex
+	ticker *time.Ticker
+
+	numAttendees int
+	attendees    map[int]eventbrite.AttendeeProfile
 }
 
 // CreateSession to interact iwht the EventBrite Event APIs. The constructed
@@ -28,18 +34,63 @@ func CreateSession(eventID int, authToken string) (*Session, error) {
 
 	log.Infof("retrieved %d attendees from eventbrite", len(attendees))
 
+	ticker := time.NewTicker(5 * time.Minute)
+	log.Info("eventbrite polling setup for 5 minute intervals")
+
 	session := &Session{
-		attendees: make(map[int]eventbrite.AttendeeProfile),
+		Mutex:  sync.Mutex{},
+		ticker: ticker,
+
+		numAttendees: len(attendees),
+		attendees:    make(map[int]eventbrite.AttendeeProfile),
 	}
 
+	go session.poll(ticker, eventID, authToken)
+
+	session.Lock()
 	for _, a := range attendees {
 		session.attendees[a.ID] = a
 	}
+	session.Unlock()
 
 	return session, nil
 }
 
+// Close will release resources
+func (s *Session) Close() {
+	log.Info("ending polling with eventbrite")
+	s.ticker.Stop()
+}
+
+// AttendeeProfile -
 func (s *Session) AttendeeProfile(id int) (*eventbrite.AttendeeProfile, bool) {
 	profile, ok := s.attendees[id]
 	return &profile, ok
+}
+
+func (s *Session) poll(ticker *time.Ticker, eventID int, authToken string) {
+	for range ticker.C {
+		attendees, err := eventbrite.GetAttendees(eventID, authToken)
+		if err != nil {
+			log.WithError(err).Warn("error polling eventbrite")
+			continue
+		}
+
+		if len(attendees) != s.numAttendees {
+			// there are updates
+			newMap := make(map[int]eventbrite.AttendeeProfile)
+			for _, a := range attendees {
+				newMap[a.ID] = a
+			}
+
+			s.Lock()
+			s.attendees = newMap
+			s.numAttendees = len(attendees)
+			s.Unlock()
+
+			log.Info("updated cached attendee list")
+		}
+
+		// no updates, continue
+	}
 }
