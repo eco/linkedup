d19d9bde50413ffc7d9301f4abfca558b1bc0b28
Hamdi Allam <hamdi.allam97@gmail.com>
2019-10-13 21:39:52 -0700

    bonus operations on the keeper

---
+++ x/longy/internal/keeper/bonus.go
@@ -0,0 +1,47 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/eco/longy/x/longy/internal/types"
+)
+
+// SetBonus -
+func (k Keeper) SetBonus(ctx sdk.Context, b *types.Bonus) {
+	key := types.BonusKey()
+
+	bz, err := k.cdc.MarshalBinaryLengthPrefixed(b)
+	if err != nil {
+		panic(err)
+	}
+
+	k.Set(ctx, key, bz)
+}
+
+// GetBonus -
+func (k Keeper) GetBonus(ctx sdk.Context) *types.Bonus {
+	key := types.BonusKey()
+	bz, _ := k.Get(ctx, key)
+	if bz == nil {
+		return nil
+	}
+
+	var b types.Bonus
+	err := k.cdc.UnmarshalBinaryLengthPrefixed(bz, &b)
+	if err != nil {
+		panic(err)
+	}
+
+	return &b
+}
+
+// ClearBonus -
+func (k Keeper) ClearBonus(ctx sdk.Context) {
+	key := types.BonusKey()
+	k.Delete(ctx, key)
+}
+
+// HasBonus -
+func (k Keeper) HasBonus(ctx sdk.Context) bool {
+	key := types.BonusKey()
+	return k.Has(ctx, key)
+}
