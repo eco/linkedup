3fa3869dd26a8502171dc2ac39a0c6e9db3e5fc6
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2019-10-30 12:56:43 -0700

    -fixing pathing

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 echo "Exporting current state of app"
-./bin/lyd export --for-zero-height > lyd_export.json
+../bin/lyd export --for-zero-height > lyd_export.json
 
 echo "Reseting the chain state to 0"
-./bin/lyd unsafe-reset-all
+../bin/lyd unsafe-reset-all
 
 echo "Moving exported state to genesis"
 cp lyd_export.json ~/.lyd/config/genesis.json
 
 echo "Restarting the chain"
-./bin/lyd start
+../bin/lyd start
 
