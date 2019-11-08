6bab4006df6e1bc4562a3a6e76309e5ecaa46667
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2019-11-07 16:45:15 -0800

    -adding raw email send to the handler for keys

--- key-service/handler/handlers.go
+++ key-service/handler/handlers.go
@@ -28,7 +28,8 @@ func Router(
 
 	registerPing(r)
 	registerKey(r, eb, mk, db, mc)
-	registerEmailManual(r, db, eb, mc)
+	registerEmailManual(r, db, eb)
+	registerInfo(r, db, mc)
 	registerIDToAddress(r)
 
 	return middleware.LogHTTP(r)
